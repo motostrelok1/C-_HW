7ae5d7c18a6539f2207f4d5289e7fe6a2c73eaa5
Alexei Dubovskoi <motostrelok1@yandex.ru>
2022-06-13 16:41:30 +0300

    hw_1_1 готов

--- HW_1_1.cs
+++ HW_1_1.cs
@@ -1,7 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("Привет, это программа сравнения двух целых, положительных чисел");
 Console.WriteLine(" Введи первое число ");
-int firstNumber =  Console.Read();
-Console.WriteLine(2+2+" "+firstNumber+" "+1+" "+2);
+int firstNumber =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введи второе число ");
+int secondNumber =  Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber )
+  {  
+    Console.WriteLine(" первое число больше второго "+firstNumber+" > "+secondNumber);
+  }else if (firstNumber<secondNumber) 
+  {
+    Console.WriteLine(" второе число больше перого "+firstNumber+" < "+secondNumber);
+  }else {Console.WriteLine(" числа равны "+secondNumber+" = "+firstNumber);}
+
+Console.WriteLine(firstNumber+" "+secondNumber);
 
 
