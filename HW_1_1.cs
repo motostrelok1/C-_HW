a3ee576450d1362f4f23472437f634fff5af9216
Alexei Dubovskoi <motostrelok1@yandex.ru>
2022-06-13 15:03:53 +0300

    init commit

---
+++ HW_1_1.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет, это программа сравнения двух целых, положительных чисел");
+Console.WriteLine(" Введи первое число ");
+int firstNumber =  Console.Read();
+Console.WriteLine(2+2+" "+firstNumber+" "+1+" "+2);
+
+
